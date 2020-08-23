35777928a9b8e17e711a0f37435689931302acb1
floreez237 <florianlowe12@gmail.com>
2020-08-24 00:42:10 +0100

    renamed base package

---
+++ src/main/java/com/florian/services/IngredientService.java
@@ -0,0 +1,15 @@
+package com.florian.services;
+
+import com.florian.commands.IngredientCommand;
+
+/**
+ * Created by jt on 6/27/17.
+ */
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(String recipeId, String ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(String recipeId, String idToDelete);
+}
---
+++ src/main/java/com/florian/services/UnitOfMeasureService.java
@@ -0,0 +1,13 @@
+package com.florian.services;
+
+import com.florian.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
